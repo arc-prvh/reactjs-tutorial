716b6942a4851c133da368abfdadbb10c38a54a0
Anweshan Roy Chowdhury <anweshan@pravaahconsulting.com>
2023-02-16 23:41:26 +0530

    TASK-2 completed

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,40 +1,36 @@
 import React, { useState, useEffect } from 'react'
-import styles from './App.module.css'
-
-const MAX_LIMIT = 5;
-const MIN_LIMIT = 0;
 
 export default function App() {
   // useState hook
-  const [ count, setCount ] = useState(0);
-
-  function incrementCount(evt) {
-    evt.preventDefault();
-    setCount( (prevValue) => prevValue + 1);
-  }
-
-  function decrementCount(evt) {
-    evt.preventDefault();
-    setCount( (prevValue) => prevValue - 1);
-  }
-
+  const [ loading, setLoading ] = useState(true);
+  const [ response, setResponse ] = useState(undefined);
+  const [ error, setError] = useState(false);
+ 
   useEffect( () => {
-    if(count > 3) {
-      return;
-    }
-    fetch('http://ip-api.com/json/24.48.0.1?fields=45674495').then( (res) => res.json() )
+    fetch(`http://ip-api.com/json/24.48.0.1?fields=45674495`).then( (res) => res.json() )
     .then( (res) => {
-      console.log(res);
+      setTimeout( () => {
+        setResponse(res);
+        setLoading(false);
+      }, 10 * 1000)
     })
     .catch( err => {
       console.error(err)
+      setError(true);
+    }).finally( () => {
+      
     });
 
   }, [])
 
   return (
-    <>
-      <p>Hello world</p>
-    </>
+    loading ? (<p>Loading....</p>) : (error ? <p>There was an error!</p> : <MyComponent response={response} loading={loading}/>)
+  )
+}
+
+
+function MyComponent({response, loading}) {
+  return (
+    <p>Country: {response.country}</p>
   )
 }
